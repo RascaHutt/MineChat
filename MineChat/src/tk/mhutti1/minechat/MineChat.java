47f9f03f760234e4e707bc1eb7aef06829fa90c1
Isaac Hutt <mhutti1@gmail.com>
2012-10-17 18:52:47 +0100

    Says Hi

--- MineChat/src/tk/mhutti1/minechat/MineChat.java
+++ MineChat/src/tk/mhutti1/minechat/MineChat.java
@@ -14,16 +14,19 @@ import org.bukkit.enchantments.EnchantmentWrapper;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 public final class MineChat extends JavaPlugin{
-	 //private static final Plugin MineChat = null;
+	 private static final Plugin MineChat = null;
+	 boolean shutdown;
 	@Override
 	    public void onEnable(){
 	        // TODO Insert logic to be performed when the plugin is enabled
 		 getLogger().info("MineChat has been initialized.");
+		 shutdown = true;
 		 try{
 			 
-		 
+		 //set the update checker url
 		 final URL url = new URL("http://www.buildcast-tinywebdb.appspot.com/update");
 		 URLConnection con = url.openConnection();
 		 InputStream in = con.getInputStream();
@@ -46,6 +49,46 @@ public final class MineChat extends JavaPlugin{
 	        {
 	           
 	        }
+		 Thread t = new Thread() {
+			    @Override
+			    public void run() {
+			        while(shutdown = true) {
+			        	try {
+							Thread.sleep(1000*5);
+						} catch (InterruptedException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
+			            try {
+			             
+			             final URL url = new URL("http://www.buildcast-tinywebdb.appspot.com/update");
+			       		 URLConnection con = url.openConnection();
+			       		 InputStream in = con.getInputStream();
+			       		 String encoding = con.getContentEncoding();
+			       		 encoding = encoding == null ? "UTF-8" : encoding;
+			       		 ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			       		 byte[] buf = new byte[8192];
+			       		 int len = 0;
+			       		 while ((len = in.read(buf)) != -1) {
+			       		     baos.write(buf, 0, len);
+			       		 }
+			       		 String body = new String(baos.toByteArray(), encoding);
+			       	  getServer().broadcastMessage(body);
+			       	    }
+			       	        catch (UnknownHostException e)
+			       	        {
+			       	           
+			       	        }
+			       	        catch (IOException e)
+			       	        {
+			       	           
+			       	        }
+			             
+			            }
+			        
+			    }
+			};
+			t.start();
 	}
 
 	 
@@ -53,6 +96,7 @@ public final class MineChat extends JavaPlugin{
 	    public void onDisable() {
 	        // TODO Insert logic to be performed when the plugin is disabled
 	    	getLogger().info("MineChat has been shutdown.");
+	    	shutdown = false;
 	    }
 	    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
 	    	if(cmd.getName().equalsIgnoreCase("diamonds")){ // If the player typed /basic then do the following...
