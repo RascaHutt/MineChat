3b2b28291bbb763cabe9c52c041dba28d2ec7d5e
mhutti1 <mhutti1@gmail.com>
2012-10-19 09:39:39 +0200

    Update buildcast-tinywebdb/main.py

--- buildcast-tinywebdb/main.py
+++ buildcast-tinywebdb/main.py
@@ -7,7 +7,8 @@
 ### component of an App Inventor app.
 
 
-### Author: David Wolber (wolber@usfca.edu), using sample of Hal Abelson
+### Original code by: David Wolber (wolber@usfca.edu), using sample of Hal Abelson
+### Web support and minecraft integration by: Isaac Hutt (mhutti1@gmail.com)
 
 import logging
 from cgi import escape
@@ -509,18 +510,3 @@ def main():
 
 if __name__ == '__main__':
   main()
-
-### Copyright 2009 Google Inc.
-###
-### Licensed under the Apache License, Version 2.0 (the "License");
-### you may not use this file except in compliance with the License.
-### You may obtain a copy of the License at
-###
-###     http://www.apache.org/licenses/LICENSE-2.0
-###
-### Unless required by applicable law or agreed to in writing, software
-### distributed under the License is distributed on an "AS IS" BASIS,
-### WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-### See the License for the specific language governing permissions and
-### limitations under the License.
-###
